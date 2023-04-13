84b701f80f505a31d736e3cfd9d333fd12565aec
Nawel Mougni <8g257pdmbr84-student@anonymous-codio.com>
2023-04-13 12:50:27 +0000

    writer implementation

--- com/hemebiotech/analytics/ISymptomWriter.java
+++ com/hemebiotech/analytics/ISymptomWriter.java
@@ -9,4 +9,6 @@ import java.util.Map;
  */
 public interface ISymptomWriter {
 
+    public void writeSymptoms(Map<String, Integer> symptoms);
+    
 }
--- com/hemebiotech/analytics/WriteSymptomDataToFile.java
+++ com/hemebiotech/analytics/WriteSymptomDataToFile.java
@@ -5,3 +5,28 @@ import java.io.IOException;
 import java.util.Iterator;
 import java.util.Map;
 
+public class WriteSymptomDataToFile implements ISymptomWriter {
+
+@Override
+	public void writeSymptoms(Map<String, Integer> symptoms) {
+		// TODO Auto-generated method stub
+		try {
+			FileWriter writer = new FileWriter("result.out");
+			symptoms.forEach((key, value) -> {
+				try {
+					writer.write(key+" : "+value+"\n");
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			});
+			writer.close();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
